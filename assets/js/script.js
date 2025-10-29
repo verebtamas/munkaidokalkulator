4c5ddccbc8c284635988ed4b6adcf30091ed7af9
Tamás | toot <53124243+verebtamas@users.noreply.github.com>
2025-10-29 08:33:15 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 
+// Letiltja a Ctrl+Shift+C (Elem vizsgálata) billentyűkombinációt
+document.addEventListener('keydown', function(event) {
+  if (event.ctrlKey && event.shiftKey && event.key === 'C') {
+      event.preventDefault();
+  }
+});
+
+// Letiltja a Ctrl+Shift+K (Konzol megnyitása) billentyűkombinációt
+document.addEventListener('keydown', function(event) {
+  if (event.ctrlKey && event.shiftKey && event.key === 'K') {
+      event.preventDefault();
+  }
+});
