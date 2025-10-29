d7c7b745742fbeb93e9cbc8102aaf78b5f1741b1
Tamás | toot <53124243+verebtamas@users.noreply.github.com>
2025-10-29 08:38:06 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+//dev tools ban
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'F12' || 
+        (event.ctrlKey && event.shiftKey && event.key === 'I') || 
+        (event.ctrlKey && event.shiftKey && event.key === 'J') || 
+        (event.ctrlKey && event.key === 'U')) {
+        event.preventDefault();
+    }
+});
+
+document.addEventListener('contextmenu', function(event) {
+  event.preventDefault();
+});
 
 // Letiltja a Ctrl+Shift+C (Elem vizsgálata) billentyűkombinációt
 document.addEventListener('keydown', function(event) {
